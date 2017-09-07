f4f07d100ca7295675e9cb61f7651029804f1dc1
chenqing512 <chenqing512@sina.com>
2017-09-07 16:13:48 +0800

    Initial Commit

---
+++ DoctorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoctorDemo
+//
+//  Created by ChenQing on 17/9/7.
+//  Copyright © 2017年 ChenQing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoctorDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DoctorDemo
+//
+//  Created by ChenQing on 17/9/7.
+//  Copyright © 2017年 ChenQing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
